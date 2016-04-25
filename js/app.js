4abd8f1710b0cee88209d557df74386bf6781f94
sarusia00 <sarus00@gmail.com>
2016-04-25 01:56:47 +0100

    css styling for smaller screeens changes

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
     removeBtn.classList.add("smallBtn");
     removeBtn.innerHTML = "Remove item";
 
-    var row = table.insertRow(2);
+    var row = table.insertRow(1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
